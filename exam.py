6f106d7d07834f683ebe4bf37a2a84bc87e5de95
Dabija Alex <dabijaalex27@gmail.com>
2024-10-11 01:26:02 +0300

    Added whole app from windows to linux subsystem ( i developed it there because it was meant for windows word documents and it was easier to test"

--- exam.py
+++ exam.py
@@ -1,77 +1,187 @@
+# Copyright 2024 Dabija Alexandru
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies 
+# or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 import random
 import os
 from docx import Document
+import shutil
+from docx.shared import Pt
+import re
+from io import BytesIO
+# from docxcompose.processor import Processor
 
 
 class Create_All_files():
     
     @staticmethod
-    def take_input_from_file(path):
+    def take_input_from_file(input_list, nr_questions_total = 9):
+
+        partition = nr_questions_total // len(input_list)
         exercitii = []
-        f = open(path, "r")
         numbers = "0123456789"
-        index_exercitii = -1
+        index_input = 0
 
-        for x in f:
-            if x[0] in numbers:
-                exercitii.append(x)
-                index_exercitii += 1
-                exercitii[index_exercitii] = "\t" + exercitii[index_exercitii]
-            else:
-                exercitii[index_exercitii] += "\t"
-                exercitii[index_exercitii] += str(x)
+        for i in range(0, len(input_list)):
+            index_exercitii = -1
+            exercitii.append([])
+            exercitii[i] = []
+            with open(input_list[index_input], 'r', encoding='utf-8') as f:
+                for x in f:
+                    if x[0] in numbers:
+                        words = re.split(r'[ \t]+', x)
+                        x = " ".join(words[1:])
+                        exercitii[i].append(x)
+                        index_exercitii += 1
+                        exercitii[i][index_exercitii] = "\t" + exercitii[i][index_exercitii]
+                    else:
+                        # exercitii[i][index_exercitii] += "\t"
+                        exercitii[i][index_exercitii] += str(x)
+                for j, x in enumerate(exercitii[i]):
+                    exercitii[i][j] = exercitii[i][j][:-1]
 
-        random.shuffle(exercitii)
 
+            random.shuffle(exercitii[i])
+            index_input += 1
         return exercitii
 
     @staticmethod
     def create_Dir(parent_dir):
-
         directory = "SubiecteWord"
         
-        # Parent Directory path 
-        # parent_dir = "."
-        
-        # Path 
         path = os.path.join(parent_dir, directory) 
         
         try:
             os.mkdir(path) 
         except FileExistsError:
             a = 1
+            shutil.rmtree(path)
+            os.mkdir(path)
+    
 
     @staticmethod
-    def init_WordDocs(path_to_folder):
-        fisiere = []
-        for i in range(1, 15):
-            file_name = (f"{path_to_folder}/SubiecteWord/SubiectNr{i}.docx")
-            fisiere.append(file_name)
-            document = Document('test.docx')
+    def take_nr_of_elems_from_index(exercitii, nr, index):
+        set = []
+        nr = int(nr)
+        for i in range(0, nr):
+            try:
+                set.append(exercitii[index + i])
+            except IndexError:
+                return []
+        return set
+
+    @staticmethod
+    def cat_questions(bold, size, name, path_to_folder, exercitii, input_list_numbers = [], header_path = "./test.docx", batch = 1):
+        index_lists = []
+        for i in range(0, len(input_list_numbers)):
+            index_lists.append(0)
+        increment_docx = 1
+        ok = 1
+        while True:
+            index_exercitiu = 1
+            file_name = (f"{path_to_folder}/SubiecteWord/SubiectNr_v{batch}_({increment_docx:03})_.docx")
+            increment_docx += 1
+
+            index_input = 0
+            cat_set = []
+
+            for x in exercitii:
+                nr = input_list_numbers[index_input]
+                set = Create_All_files.take_nr_of_elems_from_index(x, nr, index_lists[index_input])
+                if set == []:
+                    ok = 0
+                    break
+                cat_set += set
+                index_lists[index_input] += nr
+                index_input += 1
+
+            if ok == 0:
+                break
+
+
+            document = Document(header_path)
             document.save(file_name)
-        return fisiere
+
+            document = Document(file_name)
+
+            for x in cat_set:
+                p = document.add_paragraph()
+                run = p.add_run(f"{index_exercitiu}.{x}")
+                run.bold = bold
+                run.font.name = name
+                run.font.size = Pt(size)
+                index_exercitiu += 1
+            document.save(file_name)
+
 
     @staticmethod
-    def cat_questions(fisiere, exercitii):
+    def combine_word_documents(path_to_folder, files, merged_document):
+        merged_document.add_page_break()
 
-        contor = 0
-        nr_file = 0
-        document = Document(fisiere[nr_file])
-        for x in exercitii:
-            p = document.add_paragraph(style='List Number')
-            run = p.add_run(x)
-            run.bold = True
-            contor += 1
-            if contor == 9:
-                document.save(fisiere[nr_file])
-                nr_file += 1
-                document = Document(fisiere[nr_file])
-                contor = 0
+        for index, file in enumerate(files):
+            sub_doc = Document(file)
+
+            if index < len(files)-1:
+                sub_doc.add_page_break()
+
+
+            for element in sub_doc.element.body:
+                merged_document.element.body.append(element)
+
+        merged_document.save(f"{path_to_folder}/SubiecteWord/merged.docx")
+
 
     @staticmethod
-    def exec_all(path_to_folder):
-        path = "test.in"
-        Create_All_files.create_Dir(path_to_folder)
-        exercitii = Create_All_files.take_input_from_file(path)
-        fisiere = Create_All_files.init_WordDocs(path_to_folder)
-        Create_All_files.cat_questions(fisiere, exercitii)
+    def exec_all(path_to_folder, input_list = [], input_list_numbers = [], header_path = "test.docx",
+                 bold = False, size = 8, name = "Calibri", nr_of_total_files = 40):
+
+        if path_to_folder == "":
+            return False
+        try:
+            Create_All_files.create_Dir(path_to_folder)
+        except FileNotFoundError:
+            return False
+        
+        try:
+            exercitii = Create_All_files.take_input_from_file(input_list)
+        except FileNotFoundError:
+            return False
+        
+        batch = 1
+        z = 1
+        while z == 1:
+            Create_All_files.cat_questions(bold, size, name, path_to_folder, exercitii, input_list_numbers, header_path, batch)
+            for lista_exercitii in exercitii:
+                random.shuffle(lista_exercitii)
+            lista = os.listdir(f"{path_to_folder}/SubiecteWord")
+            print(lista)
+            print(len(lista))
+            if len(lista) > nr_of_total_files:
+                break
+            if len(lista) == nr_of_total_files:
+                break
+            batch += 1
+
+        list = os.listdir(f"{path_to_folder}/SubiecteWord")
+        # print(list)
+        for index, x in enumerate(list):
+            x = f"{path_to_folder}/SubiecteWord/{x}"
+            list[index] = x
+
+        doc1 = Document(list[0])
+        list = list[1:]
+        Create_All_files.combine_word_documents(path_to_folder, list, doc1)
+
+        return True
--- widget.py
+++ widget.py
@@ -1,50 +1,261 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QMessageBox, QFileDialog, QLabel
+# Copyright 2024 Dabija Alexandru
 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies 
+# or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QFileDialog, QLabel, QSizePolicy, QHBoxLayout,  QSpacerItem, QCheckBox, QLineEdit, QComboBox
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QIntValidator
+
+from MesBox import MesBox
 from exam import Create_All_files
+import storage
+from validator import FloatValidator
+# from widget2 import Widget2
+
+
+#color buttin individually
 
 class Widget(QWidget):
-    def __init__(self):
+    def __init__(self, parent):
         super().__init__()
         self.title = 'Application'
+        self.resize(500, 500)
+        self.parent = parent
+        # self.font
+        self.bold = False
+        self.font_size_pct = 8 
+        self.font_name = "Calibri"
+        self.header_path = ""
+        self.folder_path = ""
+        self.input_file = ""
+        self.nr_subiecte = 0
+
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(100, 100, 300, 200)
-
-        # Create layout
         layout = QVBoxLayout()
 
-        # Create a label to show the selected folder path
-        self.label = QLabel("Selected folder path will appear here", self)
-        layout.addWidget(self.label)
+        h_layout_path_to_folder = QHBoxLayout()
 
-        # Create a button
-        self.button = QPushButton('Choose where to place docs', self)
-        self.button.clicked.connect(self.openFolderDialog)
-        layout.addWidget(self.button)
 
-        # Set the layout for the window
+
+        path_to_folder_button = QPushButton('Choose where to place docs', self)
+        path_to_folder_button.clicked.connect(self.openFolderDialog)
+        path_to_folder_button.setStyleSheet("background-color: #888;")
+        path_to_folder_button.setMinimumWidth(200)
+        h_layout_path_to_folder.addWidget(path_to_folder_button)
+
+        self.label_path = QLabel(" Selected folder path will appear here ", self)
+        self.label_path.setStyleSheet("background-color: #888;")
+        self.label_path.setMinimumHeight(20)
+        self.label_path.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        h_layout_path_to_folder.addWidget(self.label_path)
+
+        layout.addLayout(h_layout_path_to_folder)
+
+
+
+        header_layout = QHBoxLayout()
+
+        header_button = QPushButton("Choose header doc")
+        header_button.clicked.connect(self.openFolderDialog_header_doc)
+        header_button.setStyleSheet("background-color: #888;")
+        header_button.setMinimumWidth(200)
+        header_layout.addWidget(header_button)
+
+        self.label_header_doc = QLabel(" Selected header doc path will appear here ", self)
+        self.label_header_doc.setStyleSheet("background-color: #888;")
+        self.label_header_doc.setMinimumHeight(20)
+        self.label_header_doc.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        header_layout.addWidget(self.label_header_doc)
+
+        layout.addLayout(header_layout)
+
+
+        spacer = QSpacerItem(20, 20, QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        layout.addItem(spacer)
+
+
+        input_button = QPushButton("Choose input doc")
+        input_button.clicked.connect(self.the_window)
+        input_button.setStyleSheet("background-color: #888;")
+        layout.addWidget(input_button)
+
+
+        font_row = QHBoxLayout()
+
+
+        bold = QCheckBox("Bold")
+        bold.toggled.connect(self.bold_toggled)
+        bold.setStyleSheet("background-color: #888;")
+        bold.setMinimumHeight(20)
+        bold.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+
+        font_size_label = QLabel("Write font size (.0 or .5) : ", self)
+        font_size_label.setStyleSheet("background-color: #888;")
+        font_size_label.setMinimumHeight(20)
+        font_size_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+
+        self.font_size_box = QLineEdit()
+        self.font_size_box.textChanged.connect(self.font_size)
+        self.font_size_box.setStyleSheet("background-color: #888;")
+        self.font_size_box.setMaximumHeight(20)
+        self.font_size_box.setMaximumWidth(40)
+        self.font_size_box.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        float_validator = FloatValidator()
+        self.font_size_box.setValidator(float_validator)
+
+
+        self.dropdown_names = QComboBox()
+        self.dropdown_names.addItem("Arial")
+        self.dropdown_names.addItem("Times New Roman")
+        self.dropdown_names.addItem("Calibri")
+        self.dropdown_names.addItem("Verdana")
+        self.dropdown_names.addItem("Tahoma")
+        self.dropdown_names.addItem("Georgia")
+        self.dropdown_names.addItem("Courier New")
+        self.dropdown_names.addItem("Comic Sans MS")
+        self.dropdown_names.addItem("Trebuchet MS")
+        self.dropdown_names.addItem("Lucida Console")
+        self.dropdown_names.addItem("Impact")
+        self.dropdown_names.addItem("Palatino Linotype")
+        self.dropdown_names.addItem("Garamond")
+        self.dropdown_names.addItem("Bookman Old Style")
+        self.dropdown_names.addItem("Century Gothic")
+        self.dropdown_names.addItem("Franklin Gothic Medium")
+        self.dropdown_names.addItem("Gill Sans MT")
+        self.dropdown_names.addItem("Lucida Sans Unicode")
+        self.dropdown_names.addItem("Arial Black")
+        self.dropdown_names.addItem("Tahoma")
+        self.dropdown_names.addItem("Symbol")
+        self.dropdown_names.addItem("Wingdings")
+        self.dropdown_names.addItem("Cambria")
+        self.dropdown_names.addItem("Constantia")
+        self.dropdown_names.addItem("Corbel")
+        self.dropdown_names.addItem("Candara")
+        self.dropdown_names.addItem("Rockwell")
+        self.dropdown_names.addItem("Segoe UI")
+        self.dropdown_names.addItem("Open Sans")
+        self.dropdown_names.addItem("Roboto")
+
+        self.dropdown_names.currentIndexChanged.connect(self.name)
+        self.dropdown_names.setStyleSheet("background-color: #888;")
+        self.dropdown_names.setMaximumHeight(20)
+        self.dropdown_names.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+
+        spacer = QSpacerItem(50, 20, QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+
+        font_row.setAlignment(Qt.AlignLeft)
+
+
+
+
+
+        font_row.addWidget(bold)
+        font_row.addItem(spacer)
+
+        font_row.addWidget(font_size_label)
+        font_row.addWidget(self.font_size_box)
+
+        font_row.addItem(spacer)
+        font_row.addWidget(self.dropdown_names)
+
+        layout.addLayout(font_row)
+
+
+
+        spacer = QSpacerItem(20, 20, QSizePolicy.Fixed, QSizePolicy.Fixed)
+        layout.addItem(spacer)
+
+
+        nr_sub_row = QHBoxLayout()
+
+        nr_subiecte_label = QLabel("How many tests? : ", self)
+        nr_subiecte_label.setStyleSheet("background-color: #888;")
+        nr_subiecte_label.setMinimumHeight(20)
+        nr_subiecte_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+
+        self.nr_subiecte_label_box = QLineEdit()
+        self.nr_subiecte_label_box.textChanged.connect(self.get_nr_subiecte)
+        self.nr_subiecte_label_box.setStyleSheet("background-color: #888;")
+        self.nr_subiecte_label_box.setMaximumHeight(20)
+        self.nr_subiecte_label_box.setMaximumWidth(40)
+        self.nr_subiecte_label_box.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        int_validator = QIntValidator(0, 1000)
+        self.nr_subiecte_label_box.setValidator(int_validator)
+
+        nr_sub_row.addWidget(nr_subiecte_label)
+        nr_sub_row.addWidget(self.nr_subiecte_label_box)
+
+        nr_sub_row.setAlignment(Qt.AlignLeft)
+
+        layout.addLayout(nr_sub_row)
+
+
+
+
+
+        spacer = QSpacerItem(20, 40, QSizePolicy.Fixed, QSizePolicy.Fixed)
+        layout.addItem(spacer)
+
+
+        execute_button = QPushButton("Execute")
+        execute_button.clicked.connect(self.create_words)
+        execute_button.setStyleSheet("background-color: #888;")
+        layout.addWidget(execute_button)
+
+
+        layout.setAlignment(Qt.AlignTop)
+
         self.setLayout(layout)
 
-        # Variable to store the folder path
-        self.folder_path = ""
-        self.input_file = ""
 
-        self.button2 = QPushButton("Choose input doc")
-        self.button2.clicked.connect(self.openFolderDialog_Input)
-        layout.addWidget(self.button2)
-
-
-        self.button3 = QPushButton("Execute")
-        self.button3.clicked.connect(self.create_words)
-        layout.addWidget(self.button3)
+    def get_nr_subiecte(self):
+        self.nr_subiecte = int(self.nr_subiecte_label_box.text())
 
 
 
+    def name(self):
+        self.font_name = self.dropdown_names.currentText()
 
-    def print_path_to_set_folder(self):
-        print(self.folder_path)
+
+    def font_size(self):
+        x = self.font_size_box.text()
+        if x != "":
+            x = float(x)
+        else:
+            x = float(8)
+
+        self.font_size_pct = x
+
+
+    def bold_toggled(self, checked):
+        if (checked):
+            self.bold = True
+        else:
+            self.bold = False
+
+
+    def the_window(self):
+        self.parent.switch_screen()
 
 
     def openFolderDialog(self):
@@ -53,16 +264,17 @@ class Widget(QWidget):
         folder = QFileDialog.getExistingDirectory(self, "Select Folder", "", options=options)
         if folder:
             self.folder_path = folder
-            self.label.setText(f"Selected folder path: {self.folder_path}")
+            self.label_path.setText(f"Selected folder path: {self.folder_path}")
 
+    def openFolderDialog_header_doc(self):
+        header = QFileDialog.getOpenFileName(self, "Select Folder", "")
+        if header:
+            self.header_path = header[0]
+            self.label_header_doc.setText(f"Selected header doc path: {self.header_path}")
 
-    def openFolderDialog_Input(self):
-        # options = QFileDialog.Options()
-        # options |= QFileDialog()
-        file = QFileDialog.getOpenFileName(self, "Select File", "")
-        self.input_file = file
 
     def create_words(self):
-        print("Clicked")
-        Create_All_files.exec_all(self.folder_path)
-    
+        success = Create_All_files.exec_all(self.folder_path, storage.input_list, storage.input_list_numbers, self.header_path,
+                                            self.bold, self.font_size_pct, self.font_name, self.nr_subiecte)
+        mes_box = MesBox()
+        mes_box.display(success, self)
